dcffd4dba62ea4af10849f34ae2bc93a2959902f
Makhmud Islamov <sunnatovichvv@gmail.com>
2018-10-25 22:15:10 -0700

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Barbarian Mogul on 10/25/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
